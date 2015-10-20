9a44bc439750ed697be14bf5f4783ba919fd0a03
Seth <sethkara@buffalo.edu>
2015-10-20 16:31:17 -0400

    Updated for newest available classes.   Now supports missing room numbers and times.

--- parse.py
+++ parse.py
@@ -2,8 +2,8 @@ from scraper import get_info
 from app import db, UBClasses, UBRecitation
 import re
 
-
-given_url = 'http://www.buffalo.edu/class-schedule?switch=showcourses&semester=fall&division=UGRD&dept=CSE'
+#given_url = 'http://www.buffalo.edu/class-schedule?switch=showcourses&semester=fall&division=UGRD&dept=CSE'
+given_url = 'http://www.buffalo.edu/class-schedule?switch=showcourses&semester=spring&division=UGRD&dept=CSE'
 dep_abv = ''
 dep_name = ''
 course_num = []
@@ -39,83 +39,71 @@ def get_dep_name(info):
 def isolate_data(info):
     data = info
     data = re.sub("'(.*)'(.*)'(.*)'~\d+ Courses~", '', data)
-    data = re.sub('Class~Course~Title~Section~Type~Days~Time~Room~Location~Instructor \(\*\) additional instructors~Status', '', data)
+    data = re.sub(
+        'Class~Course~Title~Section~Type~Days~Time~Room~Location~Instructor \(\*\) additional instructors~Status', '',
+        data)
     data = re.sub('~~', '~', data)
     data = re.sub('~-~', ' - ', data)
     position = 0
     time_unknown = 0
     curr_data = ''
+    print data
     for i in range(len(data)):
-        if(data[i] == '~'):
-            if(position == 1):
+        if (data[i] == '~'):
+            if (position == 1):
                 course_id.append(curr_data)
-            elif(position == 2):
-                curr_data = curr_data.replace("L","")
-                curr_data = curr_data.replace("R","")
-                curr_data = curr_data.replace("B","")
-                curr_data = curr_data.replace("T","")
-                curr_data = curr_data.replace("U","")
-                curr_data = curr_data.replace(" ","")
-                print curr_data
+            elif (position == 2):
+                curr_data = curr_data.replace("L", "")
+                curr_data = curr_data.replace("R", "")
+                curr_data = curr_data.replace("B", "")
+                curr_data = curr_data.replace("T", "")
+                curr_data = curr_data.replace("U", "")
+                curr_data = curr_data.replace(" ", "")
                 course_num.append(curr_data)
-            elif(position == 3):
+            elif (position == 3):
                 course_name.append(curr_data)
-            elif(position == 4):
+            elif (position == 4):
                 section.append(curr_data)
-            elif(position == 5):
+            elif (position == 5):
                 type.append(curr_data)
-            elif(position == 6):
+            elif (position == 6):
                 days.append(curr_data)
-            elif(position == 7):
-                time.append(curr_data)
-                if(curr_data == 'TBA'):
-                    time_unknown = 1
-            elif(position == 8):
-                if(time_unknown == 0):
-                    room.append(curr_data)
+            elif (position == 7):
+                if (curr_data == 'TBA'):
+                    time.append('TBA')
+                    room.append('ARR')
+                    position += 1
                 else:
-                    if(curr_data == 'ARR'):
-                        room.append(curr_data)
-                    else:
-                        room.append("-")
-                        position += 1
-                time_unknown = 0
-            elif(position == 9):
+                    time.append(curr_data)
+            elif (position == 8):
+                if 'North' in curr_data:
+                    room.append("TBA")
+                    campus.append(curr_data)
+                    position += 1
+                else:
+                    room.append(curr_data)
+            elif (position == 9):
                 campus.append(curr_data)
-            elif(position == 10):
+            elif (position == 10):
                 instructor.append(curr_data)
-            elif(position == 11):
-                status.append(curr_data)
-            if(position >= 11):
+            elif (position == 11):
+                if ("Open" in curr_data) or ("Closed" in curr_data):
+                    status.append(curr_data)
+                else:
+                    status.append("Status")
+                    position -=1
+            if (position >= 11):
                 position = 1
             else:
                 position += 1
             curr_data = ''
 
-
-            # UBCLASS = course_name[i]
-            # DEPARTMENT = "CSE"
-            # SECTION = section[i]
-            # TYPE = type[i]
-            # DAYS = days[i]
-            # TIME = time[i]
-            # BUILDING = room[i]
-            # ROOM_NUMBER = room[i]
-            # LOCATION = campus[i]
-            # PROFESSOR_ID = 0
-            # PROFESSOR = instructor[i]
-            # STATUS = status[i]
-            # RESERVED = "No"
-            # SEMESTER = "Fall"
-            # class1 = UBClasses(UBCLASS,HUB_ID,DEPARTMENT,SECTION,TYPE,DAYS,TIME,BUILDING,ROOM_NUMBER,LOCATION,0,PROFESSOR,STATUS,RESERVED,SEMESTER)
-            # db.add(class1)
-            # db.commit()
         else:
             curr_data += data[i]
 
-    #TODO for loop making an object and adding to the database
+    # TODO for loop making an object and adding to the database
     max = 0
-    for z in range(0,60):
+    for z in range(0, len(course_num)-1):
         UBCLASS = course_num[z]
         HUB_ID = course_id[z]
         TITLE = course_name[z]
@@ -137,8 +125,10 @@ def isolate_data(info):
         PRE4 = "None"
         PRE5 = "None"
         DEGREE = "None"
+        print UBCLASS +"/"+ HUB_ID+"/"+ TITLE+"/"+DEPARTMENT+"/"+ SECTION+"/"+ TYPE+"/"+ DAYS+"/"+ TIME+"/"+ BUILDING+"/"+ ROOM_NUMBER+"/"+ LOCATION+"/"+ PROFESSOR+"/"+ STATUS+"/"+ RESERVED+"/"+SEMESTER
         if TYPE == 'LEC':
-            class1 = UBClasses(UBCLASS,TITLE,DEPARTMENT,SECTION,TYPE,DAYS,TIME,BUILDING,ROOM_NUMBER,LOCATION,PROFESSOR,STATUS,RESERVED,SEMESTER,PRE1,PRE2,PRE3,PRE4,PRE5,DEGREE)
+            class1 = UBClasses(UBCLASS, TITLE, DEPARTMENT, SECTION, TYPE, DAYS, TIME, BUILDING, ROOM_NUMBER, LOCATION,
+                               PROFESSOR, STATUS, RESERVED, SEMESTER, PRE1, PRE2, PRE3, PRE4, PRE5, DEGREE)
             db.session.add(class1)
             db.session.commit()
         else:
@@ -147,15 +137,15 @@ def isolate_data(info):
                 temp_max = result.ID
                 if temp_max > max:
                     max = temp_max
-            print max
-            class1 = UBRecitation(UBCLASS,HUB_ID,max,SECTION,TYPE,DAYS,TIME,BUILDING,ROOM_NUMBER,LOCATION,STATUS,RESERVED,SEMESTER)
+            #print max
+            class1 = UBRecitation(UBCLASS, HUB_ID, max, SECTION, TYPE, DAYS, TIME, BUILDING, ROOM_NUMBER, LOCATION,
+                                  STATUS, RESERVED, SEMESTER)
             db.session.add(class1)
             db.session.commit()
     return data
 
 
 def get_course_id(data):
-
     return
 
 
@@ -163,8 +153,9 @@ def get_course_num(info):
     global course_num
     data = isolate_data(info)
 
-    print data
+    #print data
     return course_num
+
 
 stuff = get_info(given_url)
 print stuff
