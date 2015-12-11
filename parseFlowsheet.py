6995148331a08413637794ea6597f2dd7ae2ec31
Seth <sethkara@buffalo.edu>
2015-12-11 13:47:57 -0500

    Parser to filter out the degree flowsheets we want to scrape and parse.  This handles multiple Gen Eds and detailed error checking.
    Adds Degree table entry for every class.
    
    Does not include credits or title.

--- parseFlowsheet.py
+++ parseFlowsheet.py
@@ -1,50 +1,66 @@
-# from scrapeFlowsheet import get_flowsheet_info
-# #from app import db, UBClasses, UBRecitation
-# import re
-#
-# flowsheet_url = 'https://www.eng.buffalo.edu/undergrad/advisement/flowsheets/?flow=CSEBS11&title=Computer+Science+BS+-+Students+entering+CS+Fall+2011+or+Later'
-# degree = []    # ComputerScienceBS, MechanicalEngineeringBS, etc.  All entries are the same for each 'flowsheet_url'
-# semester = []  # Integer 1...8
-# course = []    # CSE115, MTH141, Gen Ed, etc.
-#
-# def isolate_flowsheet_data(flowsheet_info):
-#     # split flowsheet_info into a large array, with elements delimited by \n.
-#     # example: flowsheet_data = [CSE115 MTH141 ... END OF SEMESTER CSE116...]
-#     flowsheet_data = flowsheet_info.split("\n")
-#
-#
-#     semesterCounter = 1
-#     for item in flowsheet_data:
-#         if item != 'END OF SEMESTER':
-#             semester.append(semesterCounter)
-#             course.append(item)
-#         else:
-#             semesterCounter = semesterCounter+1
-#             if semesterCounter == 9:
-#                 break
-#     print '**************************************************\n\n'
-#     print semester
-#     print course
-#
-#
-#
-#
-#
-# flowsheetInfo = get_flowsheet_info(flowsheet_url)
-# isolate_flowsheet_data(flowsheetInfo)
-# #print flowsheetInfo
 from scrapeFlowsheet import get_flowsheet_info
 from scraper import get_url
-#from app import db, UBClasses, UBRecitation
+from app import db, UBClasses, UBRecitation, Degree
 import re
 
 base_url = 'https://www.eng.buffalo.edu/undergrad/advisement/flowsheets/'
 path = '//*[@id="div_content"]/ul/li/a'
 
 links = get_url(base_url, path)
-for link in links:
-    sheet_url = base_url + link
+modified_links = []
+for i in range(0,len(links)):
+    if i is 1:
+        modified_links.append(links[i])
+    elif i is 4:
+        modified_links.append(links[i])
+    elif i is 5:
+        modified_links.append(links[i])
+    elif i is 6:
+        modified_links.append(links[i])
+    elif i is 7:
+        modified_links.append(links[i])
+    elif i is 9:
+        modified_links.append(links[i])
+    elif i is 11:
+        modified_links.append(links[i])
+    elif i is 14:
+        modified_links.append(links[i])
+    elif i is 15:
+        modified_links.append(links[i])
+    elif i is 17:
+        modified_links.append(links[i])
+    else:
+        pass
+print modified_links
+
+for modified_links in modified_links:
+    degree = modified_links[6:]
+    degree = ''.join([i for i in degree if not i.isdigit()])
+    if degree == 'CSEBS':
+        degree = 'CSE'
+    if degree == 'CEN':
+        pass
+    #print degree
+    sheet_url = base_url + modified_links
     sheet = get_flowsheet_info(sheet_url)
+    #print sheet
+    sem_index = 0
+    gen_ed_counter = 0
+    for semester in sheet:
+        sem_index += 1
+        for j in range(1, len(semester)):
+            if str(semester[j]).find(' or ') != -1:
+                index = semester[j].index(' or ')
+                semester[j] = semester [j][:index]
+            if str(semester[j]).find('Gen Ed') != -1:
+                gen_ed_counter += 1
+                semester[j] = semester[j] + ' ' + str(gen_ed_counter)
+            degree_entry = Degree(degree,semester[j], '', sem_index, 4)
+            db.session.add(degree_entry)
+            db.session.commit()
+            print str(degree_entry.DEGREE_NAME) + ' - ' + str(degree_entry.UBCLASS)  + ' - ' + str(degree_entry.SEM_INDEX)
+
+
 
 
 
